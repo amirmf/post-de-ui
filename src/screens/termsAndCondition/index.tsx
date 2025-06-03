7504c0a1cdb189befed77024a0445e6991bf4209
Amir Masourifard <amir@Amirs-MacBook-Pro.local>
2025-06-03 16:52:51 +0330

    Initial commit2

--- src/screens/termsAndCondition/index.tsx
+++ src/screens/termsAndCondition/index.tsx
@@ -115,15 +115,14 @@ zu deren Tochtergesellschaften oder zu anderen Zustelldiensten.
 
 **3. Zahlungsbedingungen, Preise und Rechnung**
 
-a\. Alle Preise sind Bruttopreise inkl. Umsatzsteuer in der jeweils gültigen Höhe.
 
-b\. Der Kunde hat die vereinbarten Entgelte durch Überweisung zu entrichten. Nach Vertragsschluss ist eine Änderung der Zahlungsart nicht mehr möglich.
+a\. Der Kunde hat die vereinbarten Entgelte durch Überweisung zu entrichten. Nach Vertragsschluss ist eine Änderung der Zahlungsart nicht mehr möglich.
 
-c\. Der Kunde ist damit einverstanden, dass ihm die Rechnung im PDF-Format an die angegebene E-Mailadresse zugestellt wird.
+b\. Der Kunde ist damit einverstanden, dass ihm die Rechnung im PDF-Format an die angegebene E-Mailadresse zugestellt wird.
 
-d\. Die Rechnung ist innerhalb von 7 Tagen nach Zugang zur Zahlung fällig. Nach Ablauf dieser Frist gerät der Kunde gemäß § 286 Abs. 3 BGB automatisch in Verzug, ohne dass es einer weiteren Mahnung bedarf. 
+c\. Die Rechnung ist innerhalb von 7 Tagen nach Zugang zur Zahlung fällig. Nach Ablauf dieser Frist gerät der Kunde gemäß § 286 Abs. 3 BGB automatisch in Verzug, ohne dass es einer weiteren Mahnung bedarf. 
 
-e\. Die Zahlungen sind auf ein Konto mit Sitz in den Vereinigten Arabischen Emiraten zu leisten. Der Kunde ist dafür verantwortlich, dass die Überweisung ordnungsgemäß und vollständig unter Berücksichtigung etwaiger Bankgebühren erfolgt. Ein entsprechender Zahlungsempfänger sowie die Kontodaten werden dem Kunden auf der Rechnung mitgeteilt. 
+d\. Die Zahlungen sind auf ein niederländisches Konto der Digital Service Solutions LLC zu leisten. Der Kunde ist dafür verantwortlich, dass die Überweisung ordnungsgemäß und vollständig unter Berücksichtigung etwaiger Bankgebühren erfolgt. Der entsprechende Zahlungsempfänger sowie die Kontodaten werden dem Kunden auf der Rechnung mitgeteilt.
 
 **4. Pflichten des Nutzers**
 
